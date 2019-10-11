e0f47529d0b9c3164ac319d807585cdbcdb2e2c8
dante <c.dante.federici@gmail.com>
2019-10-11 17:24:18 -0400

    Removed stateful component example.

--- src/components/timeAndCount.js
+++ src/components/timeAndCount.js
@@ -21,36 +21,6 @@ const Layout = ({ timeDisplay, value, increment }) => html`
 		<button onClick=${increment}> Increment</button>
 	</div>`;
 
-// Standard Claas Component
-export class TimeAndCount extends Component {
-	state = {
-		time: Date.now(),
-		value: this.props.initialCount,
-	}
-
-	// Only run the timer while the component is on the screen
-	componentDidMount() {
-		this.timer = setInterval(() => {
-			this.setState(produce((draft) => { draft.time = Date.now(); }));
-		}, 1000);
-	}
-
-	// Clear the timer when we are off the screen
-	componentWillUnmount() {
-		clearInterval(this.timer);
-	}
-
-	increment = () => this.setState(produce((draft) => { draft.value += 1; }))
-
-	render(props, state) {
-		const uiState = produce(state, (draft) => {
-			draft.timeDisplay = new Date(state.time).toLocaleTimeString();
-			draft.increment = this.increment;
-		});
-		return html`<${Layout} ...${uiState} />`;
-	}
-}
-
 // Refract component
 const aperture = (component, initialProps) => {
 	const { initialCount } = initialProps;
@@ -77,31 +47,6 @@ const aperture = (component, initialProps) => {
 const handler = (initialProps) => (effect) => {};
 export const TimeAndCountWithEffects = withEffects(aperture, { handler })(Layout);
 
-// Functional Component with hooks broken out
-const useClock = () => {
-	const [time, setTime] = useState(new Date());
-
-	useEffect(() => {
-		const timer = setInterval(() => {
-			setTime(new Date());
-		}, 1000);
-
-		return () => clearInterval(timer);
-	}, []);
-
-	return time;
-};
-export const TimeAndCountHooks = (props) => {
-	const time = useClock();
-	const [count, setCount] = useState(props.initialCount);
-	const increment = () => setCount((c) => c + 1);
-	const uiState = {
-		timeDisplay: new Date(time).toLocaleTimeString(),
-		value: count,
-		increment,
-	};
-	return html`<${Layout} ...${uiState} />`;
-};
 
 // Functional Component with hooks
 export const TimeAndCountInternalHooks = (props) => {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import css from 'csz';
+// N.B. babel preproceses html tag => h() calls during build
 import { h, render } from 'preact'; // eslint-disable-line no-unused-vars
 import { html } from 'htm/preact';
 import {
-	Hello, TimeAndCount, TimeAndCountWithEffects, TimeAndCountHooks, TimeAndCountInternalHooks,
+	Hello, TimeAndCountWithEffects, TimeAndCountInternalHooks,
 } from './components/timeAndCount';
 
 // load css must be absolute path
@@ -11,12 +12,8 @@ const appClass = css`/index.css`;
 const app = html`
 	<div class='${appClass}'>
 		<h1><${Hello} name="JP" /></h1>
-		<h2>Preact</h2>
-		<${TimeAndCount} initialCount=${3} />
 		<h2>Refract</h2>
 		<${TimeAndCountWithEffects} initialCount=${4} />
-		<h2>Abstracted hook</h2>
-		<${TimeAndCountHooks} initialCount=${5} />
 		<h2>Internal hook</h2>
 		<${TimeAndCountInternalHooks} initialCount=${6} />
 	</div>`;
